bd6a2130a1dcf03c53b6be29fac1b1f234d41deb
Peter Steinberger <steipete@gmail.com>
2015-06-01 08:22:11 +0200

    add comment

--- samples/PSPDFCatalog/PSPDFCatalog/Catalog/Subclassing/CustomPDFDocument.cs
+++ samples/PSPDFCatalog/PSPDFCatalog/Catalog/Subclassing/CustomPDFDocument.cs
@@ -16,6 +16,7 @@ namespace PSPDFCatalog
 			return UIKit.UIColor.White;
 		}
 
+		// Force-disable the text copying, even though the PDF permissions would allow this.
 		public override bool AllowsCopying {
 			get {
 				return false;
