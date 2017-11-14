f23116bf810d160070822b2b750d910e1af4ad0c
Alex Soto <alex@alexsoto.me>
2017-11-14 09:44:54 -0600

    Update adonit stylus drivers support

--- StylusDrivers/xambindings/PSPDFAdonitStylusDriver/AdonitSDK.linkwith.cs
+++ StylusDrivers/xambindings/PSPDFAdonitStylusDriver/AdonitSDK.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("AdonitSDK", LinkTarget.ArmV7 | LinkTarget.Simulator64 | LinkTarget.Simulator | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("AdonitSDK", LinkTarget.ArmV7 | LinkTarget.Simulator64 | LinkTarget.Simulator | LinkTarget.Arm64, LinkerFlags = "-ObjC", Frameworks = "CoreBluetooth SystemConfiguration CoreMotion", SmartLink = false, ForceLoad = true)]
