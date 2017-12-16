d933f35a7a42bf363a87908d34399b57964002a9
Alex Soto <alex@alexsoto.me>
2017-12-16 17:29:10 -0600

    [PSPDFCatalog] Add example usage of 'IPSPDFViewControllerDelegate' to Annotations From Code sample.

--- Examples/PSPDFCatalog/PSPDFCatalog/Catalog/Annotations/AnnotationsFromCodeViewController.cs
+++ Examples/PSPDFCatalog/PSPDFCatalog/Catalog/Annotations/AnnotationsFromCodeViewController.cs
@@ -8,11 +8,15 @@ using PSPDFKit.UI;
 using UIKit;
 
 namespace PSPDFCatalog {
-	public class AnnotationsFromCodeViewController : PSPDFViewController {
+	public class AnnotationsFromCodeViewController : PSPDFViewController, IPSPDFViewControllerDelegate {
 		PSPDFAnnotation [] annotationsArr;
 
 		public AnnotationsFromCodeViewController (NSUrl document) : base (new PSPDFDocument (document))
 		{
+			// Sets the Delegate to listen for 'IPSPDFViewControllerDelegate' events like 'DidSelectAnnotations'.
+			// Any NSObject derived class can be set to 'Delegate' property as long as it implements 'IPSPDFViewControllerDelegate'.
+			Delegate = this;
+
 			Document.Title = "Programmatically create annotations";
 			Document.AnnotationSaveMode = PSPDFAnnotationSaveMode.Disabled;
 
@@ -52,6 +56,19 @@ namespace PSPDFCatalog {
 
 			annotationsArr = annotationsList.ToArray ();
 			Document.AddAnnotations (annotationsArr, options: null);
+		}
+
+		// Comes from 'IPSPDFViewControllerDelegate'. This Called after an annotation has been selected by a touch directly on the page view.
+		// https://pspdfkit.com/api/ios/Protocols/PSPDFViewControllerDelegate.html#/c:objc(pl)PSPDFViewControllerDelegate(im)pdfViewController:didSelectAnnotations:onPageView:
+		[Export ("pdfViewController:didSelectAnnotations:onPageView:")]
+		public void DidSelectAnnotations (PSPDFViewController pdfController, PSPDFAnnotation [] annotations, PSPDFPageView pageView)
+		{
+			if (annotations == null)
+				return;
+
+			foreach (var annotation in annotations) {
+				Console.WriteLine ($"Selected Annotation: {annotation.Name}");
+			}
 		}
 	}
 }
