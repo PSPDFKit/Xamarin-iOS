6035824e2f5c6ebf699ad312ddc97de31e879e51
Christoph Mantler <christoph@pspdfkit.com>
2018-11-22 15:39:57 +0100

    Change message and placeholder for aler controller

--- Examples/PSPDFCatalog/PSPDFCatalog/Catalog/Subclassing/CustomBookmarkUIViewController.cs
+++ Examples/PSPDFCatalog/PSPDFCatalog/Catalog/Subclassing/CustomBookmarkUIViewController.cs
@@ -33,14 +33,15 @@ namespace PSPDFCatalog
             // We need to properly handle adding and removing bookmarks
             if (this.Document.BookmarkManager.GetBookmarkForPage((uint)this.PageIndex) == null)
             {
-                var alert = UIAlertController.Create("", "Give Your Bookmark a Name!", UIAlertControllerStyle.Alert);
+                var alert = UIAlertController.Create("", "Please name your bookmark:", UIAlertControllerStyle.Alert);
                 var action = new PSPDFGoToAction(this.PageIndex);
                 // We need a mutable copy since we can't edit the name of the bookmark otherwise
                 var mutableBookmark = new PSPDFMutableBookmark(action);
 
                 alert.AddTextField(textField =>
                 {
-                    textField.Placeholder = "Enter Bookmark Name";
+                    // This will be the dault name of the bookmark if not name is entered into the text field
+                    textField.Placeholder = string.Format("Page {0}", PageIndex+1);
                 });
 
                 var okAction = UIAlertAction.Create("OK", UIAlertActionStyle.Default, Handle =>
