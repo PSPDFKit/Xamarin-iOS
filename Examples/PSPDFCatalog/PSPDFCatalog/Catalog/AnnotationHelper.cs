636289d9ec6013f1f8acd7ef3fe67fe1fce000bd
Christoph Mantler <christoph@pspdfkit.com>
2021-02-03 16:01:38 +0100

    Add AnnotationHelper class to get sample annotations without having to waste time writing the same code over and over.

---
+++ Examples/PSPDFCatalog/PSPDFCatalog/Catalog/AnnotationHelper.cs
@@ -0,0 +1,62 @@
+using Foundation;
+using System;
+using PSPDFKit.Model;
+using UIKit;
+using CoreGraphics;
+
+namespace PSPDFCatalog {
+	// This class is used for creating sample annotations that usually take a few lines of code to generate.
+	// We define some of them here to make our life easier in other examples, where we just need a generic annotaiton of some type.
+	public class AnnotationHelper {
+		public AnnotationHelper ()
+		{
+		}
+
+		public static PSPDFAnnotation GetAnnotationOfType (PSPDFAnnotationType type, PSPDFDocument document, uint targetPage)
+		{
+			switch (type) {
+			case PSPDFAnnotationType.Ink:
+				return GetInkAnnotation (document, targetPage);
+			case PSPDFAnnotationType.Stamp:
+				return GetStampAnnotation (document, targetPage);
+			default:
+				Console.WriteLine ($"No sample annotation of type {type} available.");
+				return null;
+			}
+		}
+
+		private static PSPDFInkAnnotation GetInkAnnotation (PSPDFDocument document, uint targetPage)
+		{
+			// Ink Annotation sample
+			var inkAnnot = new PSPDFInkAnnotation ();
+			var linesArr = NSArray<NSValue>.FromNSObjects (
+				NSValue.FromCGPoint (new CGPoint (480.93079f, 596.0625f)),
+				NSValue.FromCGPoint (new CGPoint (476.8027f, 592.96881f)),
+				NSValue.FromCGPoint (new CGPoint (468.54639f, 585.75f)),
+				NSValue.FromCGPoint (new CGPoint (456.1619f, 574.40631f)),
+				NSValue.FromCGPoint (new CGPoint (436.5531f, 550.6875f)),
+				NSValue.FromCGPoint (new CGPoint (357.086f, 434.15631f)),
+				NSValue.FromCGPoint (new CGPoint (294.1315f, 359.90631f)),
+				NSValue.FromCGPoint (new CGPoint (226.01691f, 284.625f)),
+				NSValue.FromCGPoint (new CGPoint (176.4789f, 222.75f))
+		   	);
+			var lines = new NSArray<NSValue> [] { linesArr };
+
+			CGRect viewRect = UIScreen.MainScreen.Bounds;
+			var pageInfo = document.GetPageInfo (targetPage);
+
+			inkAnnot.Lines = PSPDFInkAnnotation.ConvertViewLinesToPdfLines (lines, pageInfo, viewRect);
+			inkAnnot.LineWidth = 5;
+			inkAnnot.Color = UIColor.White;
+			return inkAnnot;
+		}
+
+		private static PSPDFStampAnnotation GetStampAnnotation (PSPDFDocument document, uint targetPage)
+		{
+			var stampAnnotation = new PSPDFStampAnnotation ();
+			stampAnnotation.Title = "This is a stamp!";
+
+			return stampAnnotation;
+		}
+	}
+}
